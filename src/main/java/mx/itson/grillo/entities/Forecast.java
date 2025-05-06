aa162b4c2b7f52762fd31154656710da230bd697
icesp <icesp@LAPTOP-GA59UK9K>
2025-05-06 10:10:08 -0700

    Se inicializó este proyecto.

---
+++ src/main/java/mx/itson/grillo/entities/Forecast.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.grillo.entities;
+
+import java.util.Date;
+import mx.itson.grillo.enums.WeatherStatus;
+
+/**
+ *
+ * @author icesp
+ */
+public class Forecast {
+    
+    private Date day;
+    private int maxTemperature;
+    private int minTemperature;
+    private WeatherStatus status;
+    
+}
